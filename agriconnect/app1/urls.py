8a8cedce1c0f7a99effa2e8440edd7f7d7d1914f
okumuwan878 <pokumu987@gmail.com>
2025-12-03 10:59:43 +0300

    login and sign up

--- agriconnect/app1/urls.py
+++ agriconnect/app1/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-   
+   path('marketplace',views.home,name='home'),
+   path('sellerdash',views.sellers,name='seller'),
+   path('advisory',views.advises,name='advise'),
+   path('login', auth_views.LoginView.as_view(template_name='app1/loggy.html'), name='login'),
+    
+
+    path('signup', views.signup_view, name='signup'),
+    path('logout', auth_views.LogoutView.as_view(next_page='loggy.html'), name='logout'),
 ]
--- agriconnect/app1/views.py
+++ agriconnect/app1/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    return render (request,'app1/marketplace.html')
+
+
+def advises(request):
+    return render (request,'app1/advisory.html')
+
+
+
+def sellers(request):
+    return render (request,'app1/sellerdashboard.html')
+
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # By default, form.save() creates a user with is_staff=False
+            # 1. Add a success message to show on the login page
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! Please log in.')
+            
+            # 2. Redirect specifically to the 'login' URL name
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'app1/signny.html', {'form': form})
+
